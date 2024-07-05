0aa2655c22c8d9aeefd66e78ce9720057ad0b071
BrailyRs03 <brailyrs03@gmail.com>
2024-07-04 23:51:57 -0400

    He agregado una descripcion de cada funcion y clase las cuales se encuentran en el proyecto.

--- detection/views.py
+++ detection/views.py
@@ -3,14 +3,56 @@ from django.shortcuts import render
 from django.http import StreamingHttpResponse, JsonResponse
 from services.video.capturar_video import generate_frame, VideoCamera
 
+# Inicializa la cámara de video
 camera = VideoCamera(camera_index=0)
 
 def home(request):
+    """
+    Renderiza la página principal.
+
+    Esta vista maneja la solicitud HTTP para la página principal de la aplicación.
+    Toma el objeto de la solicitud HTTP como parámetro y retorna una respuesta HTTP
+    que contiene el contenido de la plantilla 'index.html'. La función renderiza 
+    esta plantilla y la devuelve como respuesta.
+    
+    Retorna:
+    - HttpResponse: La respuesta HTTP con el contenido de 'index.html'.
+    """
+    
     return render(request, 'index.html')
 
 def video_feed(request):
+    """
+    Proporciona el stream de video de la cámara.
+
+    Esta vista maneja la solicitud HTTP para el stream de video. Utiliza la 
+    función generate_frame para capturar y procesar los frames de video en 
+    tiempo real. La respuesta HTTP es un StreamingHttpResponse que contiene 
+    el stream de video, con el tipo de contenido adecuado para la transmisión 
+    de video en múltiples partes.
+    
+    Retorna:
+    - StreamingHttpResponse: La respuesta HTTP que contiene el stream de video.
+    """
+    
     return StreamingHttpResponse(generate_frame(camera), content_type='multipart/x-mixed-replace; boundary=frame')
 
 def get_detections(request):
+    """
+    Devuelve las detecciones actuales de objetos en formato JSON.
+
+    Esta vista maneja la solicitud HTTP para obtener las detecciones actuales 
+    de objetos. La función obtiene las detecciones desde la cámara y las 
+    devuelve en una respuesta JSON. Esto permite a otras partes de la 
+    aplicación (como el frontend) acceder a los datos de detección en un 
+    formato estructurado y fácil de usar.
+
+    Parámetros:
+    - request (HttpRequest): El objeto de la solicitud HTTP.
+    
+    Retorna:
+    - JsonResponse: La respuesta HTTP en formato JSON que contiene las detecciones.
+    """
+    
     detections = camera.get_detections()
     return JsonResponse(detections, safe=False)
--- services/video/capturar_video.py
+++ services/video/capturar_video.py
@@ -9,6 +9,16 @@ class_names = ['Metal', 'Vidrio', 'Plastico', 'Carton', 'Medicinal']
 
 class VideoCamera(object):
     def __init__(self, camera_index=0):  # Cambiar a 2 para usar DroidCam y 0 para la cámara por defecto
+        """
+        Inicializa la cámara de video.
+        
+        Parámetros:
+        - camera_index (int): Índice de la cámara a utilizar. 0 para la cámara por defecto, 2 para DroidCam.
+        
+        Acciones:
+        - Configura la resolución de la cámara.
+        - Inicia un hilo para actualizar los frames de la cámara continuamente.
+        """
         self.video = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
         self.video.set(3, 1280)
         self.video.set(4, 720)
@@ -18,9 +28,22 @@ class VideoCamera(object):
         threading.Thread(target=self.update, args=()).start()
 
     def __del__(self):
+        """ Libera la cámara cuando el objeto es destruido. """
         self.video.release()
 
     def get_frame(self):
+        """
+        Captura un frame de la cámara y realiza la detección de objetos.
+        
+        Retorna:
+        - jpeg (bytes): El frame procesado en formato JPEG.
+        
+        Acciones:
+        - Captura un frame de la cámara.
+        - Realiza detección de objetos en el frame usando el modelo YOLO.
+        - Dibuja rectángulos y etiquetas sobre los objetos detectados.
+        - Convierte el frame procesado a formato JPEG.
+        """
         with self.lock:
             image = self.frame.copy()
         results = model(image, stream=True, verbose=False)
@@ -46,16 +69,44 @@ class VideoCamera(object):
         return jpeg.tobytes()
 
     def get_detections(self):
+        """
+        Obtiene las detecciones actuales de objetos.
+        
+        Retorna:
+        - detections (list): Lista de diccionarios con las detecciones actuales.
+        
+        Acciones:
+        - Devuelve una copia de las detecciones actuales almacenadas en el objeto.
+        """
         with self.lock:
             return self.detections if self.detections else []
 
     def update(self):
+        """
+        Actualiza continuamente los frames de la cámara en un hilo separado.
+        
+        Acciones:
+        - Captura frames de la cámara en un bucle infinito y actualiza la variable `self.frame`.
+        """
         while True:
             (self.grabbed, self.frame) = self.video.read()
             with self.lock:
                 self.frame = self.frame
 
 def generate_frame(camera: VideoCamera):
+    """
+    Genera un stream continuo de frames JPEG.
+    
+    Parámetros:
+    - camera (VideoCamera): Objeto VideoCamera para capturar los frames.
+    
+    Retorna:
+    - frame (bytes): Stream continuo de frames JPEG con el formato requerido para la transmisión HTTP.
+    
+    Acciones:
+    - Captura y codifica continuamente los frames de la cámara en formato JPEG.
+    - Formatea los frames para la transmisión HTTP.
+    """
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
